358f0364c669e37b06fe5dc7317266f1c4abdd4e
Denys <dzamota@users.noreply.github.com>
2020-05-11 10:46:20 +0300

    Updates

---
+++ precache-manifest.c7a310e7871df470c8de0fefe176269f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2f9e2d9c2571a85a8e176f3ff3cd532",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "acf6e1ec3e5694bd9398",
+    "url": "/goit-react-hw-02-feedback/static/js/2.0054db5f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.0054db5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bbc0cf270e0677cfbbb",
+    "url": "/goit-react-hw-02-feedback/static/js/main.2b73c489.chunk.js"
+  },
+  {
+    "revision": "5f76858bf903340caf72",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.50268c19.js"
+  }
+]);
